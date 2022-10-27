02992ce41f9127796b9ed8cffc6da24e4f7eb103
bax <dev.lasota@gmail.com>
2022-10-27 17:05:05 +0200

    create-next-app setup

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook Clone</title>
+      </Head>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
