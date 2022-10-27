5d63b1c13e2b3e7a1028ea4fc29fa515d9f99e98
bax <dev.lasota@gmail.com>
2022-10-27 22:47:25 +0200

    Header.js makup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
